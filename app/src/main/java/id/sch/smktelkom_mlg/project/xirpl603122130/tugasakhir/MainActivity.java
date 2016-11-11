964b60b5aed8788b15048907eeb461b40653843f
alifia putri ramdhani <alifiaputri.ramadhani@gmail.com>
2016-11-11 14:49:21 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl603122130/tugasakhir/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl603122130.tugasakhir;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
